8f1d976c1c37124cbdeb3620843268c27e1d816f
Paltn <>
2018-08-17 17:20:39 +0100

    NewFragments
    Add new fragments

---
+++ spendinglist/app/src/main/java/com/example/pcardoso/spendinglist/Fragments/SeeExpensesFragment.java
@@ -0,0 +1,24 @@
+package com.example.pcardoso.spendinglist.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pcardoso.spendinglist.R;
+
+
+public class SeeExpensesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_see_expenses, container, false);
+    }
+
+
+}
